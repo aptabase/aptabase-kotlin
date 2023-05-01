b622169e17c9dcf968cd89a4f3756395196c8b5e
Dylan Llewellyn <46717769+herefishyfish@users.noreply.github.com>
2023-05-01 19:57:08 +0800

    Initial Commit

---
+++ aptabase/src/main/java/com/aptabase/Aptabase.kt
@@ -0,0 +1,121 @@
+package com.aptabase
+
+import android.content.Context
+import android.os.Build
+import org.json.JSONObject
+import java.io.BufferedWriter
+import java.io.OutputStreamWriter
+import java.net.HttpURLConnection
+import java.net.URL
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
+import java.util.TimeZone
+import java.util.UUID
+import java.util.concurrent.TimeUnit
+import java.util.concurrent.Executors
+
+class Aptabase private constructor() {
+  private val SDK_VERSION = "aptabase-kotlin@0.0.1"
+  private val SESSION_TIMEOUT: Long = TimeUnit.HOURS.toMillis(1)
+  private var appKey: String? = null
+  private var sessionId = UUID.randomUUID()
+  private var env: EnvironmentInfo? = null
+  private var lastTouched = Date()
+  private var apiURL: String? = null
+  private val threadPool = Executors.newFixedThreadPool(5)
+
+  init {
+    Runtime.getRuntime().addShutdownHook(Thread {
+      threadPool.shutdown()
+    })
+  }
+
+  private val regions =
+    mapOf(
+      "US" to "https://us.aptabase.com",
+      "EU" to "https://eu.aptabase.com",
+      "DEV" to "http://localhost:3000"
+    )
+
+  fun initialize(context: Context, appKey: String) {
+    val parts = appKey.split("-")
+    if (parts.size != 3) {
+      println("The Aptabase App Key $appKey is invalid. Tracking will be disabled.")
+      return
+    }
+
+    val region = parts[1]
+    val baseURL = regions[region] ?: regions["DEV"]!!
+
+    apiURL = "$baseURL/api/v0/event"
+    this.appKey = appKey
+    env = EnvironmentInfo.get(context)
+  }
+
+  fun trackEvent(eventName: String, props: Map<String, Any> = emptyMap<String, Any>()) {
+    appKey?.let { appKey ->
+      env?.let { env ->
+        apiURL?.let { apiURL ->
+          val now = Date()
+          if (now.time - lastTouched.time > SESSION_TIMEOUT) {
+            sessionId = UUID.randomUUID()
+          }
+
+          lastTouched = now
+
+          val body: JSONObject = JSONObject(
+            mapOf(
+              "timestamp" to dateFormatter.format(Date()),
+              "sessionId" to sessionId.toString().lowercase(),
+              "eventName" to eventName,
+              "systemProps" to mapOf(
+                "osName" to "Android",
+                "osVersion" to Build.VERSION.RELEASE,
+                "locale" to env.locale,
+                "appVersion" to env.appVersion,
+                "appBuildNumber" to env.appBuildNumber,
+                "sdkVersion" to SDK_VERSION
+              ),
+              "props" to props
+            )
+          )
+
+          threadPool.execute {
+            val url = URL(apiURL)
+            (url.openConnection() as? HttpURLConnection)?.apply {
+              requestMethod = "POST"
+              setRequestProperty("App-Key", appKey)
+              setRequestProperty("Content-Type", "application/json")
+
+              doOutput = true
+              BufferedWriter(OutputStreamWriter(outputStream)).use { writer ->
+                writer.write(body.toString())
+              }
+
+              val responseCode = responseCode
+              if (responseCode >= 300) {
+                println(
+                  "trackEvent failed with status code $responseCode: ${inputStream.bufferedReader().use { it.readText() }}"
+                )
+              }
+
+              disconnect()
+            }
+          }
+        }
+      }
+    }
+  }
+
+  private val dateFormatter: SimpleDateFormat
+    get() {
+      val formatter = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'", Locale.US)
+      formatter.timeZone = TimeZone.getTimeZone("UTC")
+      return formatter
+    }
+
+  companion object {
+    val shared = Aptabase()
+  }
+}
---
+++ example/src/main/java/com/aptabase/example/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.aptabase.example
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import com.aptabase.Aptabase
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val initButton: Button = findViewById(R.id.initButton)
+        initButton.setOnClickListener {
+            Aptabase.shared.initialize(this, "<api-key>")
+        }
+
+        val trackButton: Button = findViewById(R.id.trackButton)
+        trackButton.setOnClickListener {
+            Aptabase.shared.trackEvent("connect_click")
+        }
+
+        val trackPropertyButton: Button = findViewById(R.id.trackPropertyButton)
+        trackPropertyButton.setOnClickListener {
+            val map = mapOf<String, Any>(
+                "with_love_from" to "NativeScript"
+            )
+            Aptabase.shared.trackEvent("hello", map)
+        }
+    }
+}
