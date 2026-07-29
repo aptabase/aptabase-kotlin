87a4782087af07f2e13209fb12e9a395809710c5
Roozbeh Zarei <r.zarei@oticamp.ir>
2026-07-29 09:44:16 +0330

    Migrate Gradle build configuration to Kotlin DSL

---
+++ aptabase/build.gradle.kts
@@ -0,0 +1,58 @@
+import org.gradle.api.publish.maven.MavenPublication
+
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+    id("maven-publish")
+}
+
+android {
+    namespace = "com.aptabase"
+    compileSdk = 34
+
+    defaultConfig {
+        minSdk = 16
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro",
+            )
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+}
+
+dependencies {
+    implementation("androidx.test:monitor:1.6.1")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+}
+
+afterEvaluate {
+    publishing {
+        publications {
+            create<MavenPublication>("release") {
+                from(components["release"])
+                groupId = "com.github.aptabase"
+                artifactId = "aptabase-kotlin"
+                version = "0.0.8"
+            }
+        }
+    }
+}
